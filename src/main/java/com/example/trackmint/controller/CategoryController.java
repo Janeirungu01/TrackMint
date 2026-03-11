072c994324b62317cacc1382ab6b31cfe0b9b4ec
Janeirungu01 <jwirungu96@gmail.com>
2026-03-11 17:09:39 +0300

    Updated CategoryController with endpoints /{categoryId}/transactions and /{categoryId}/total

--- src/main/java/com/example/trackmint/controller/CategoryController.java
+++ src/main/java/com/example/trackmint/controller/CategoryController.java
@@ -2,12 +2,15 @@ package com.example.trackmint.controller;
 
 import com.example.trackmint.dto.CategoryRequest;
 import com.example.trackmint.dto.CategoryResponse;
+import com.example.trackmint.dto.TransactionResponse;
 import com.example.trackmint.security.CustomUserDetails;
 import com.example.trackmint.services.CategoryService;
+import com.example.trackmint.services.TransactionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
@@ -16,6 +19,7 @@ import java.util.List;
 public class CategoryController {
 
     private final CategoryService categoryService;
+    private final TransactionService transactionService;
 
     @PostMapping
     public CategoryResponse createCategory(
@@ -32,6 +36,24 @@ public class CategoryController {
 
         Long userId = userDetails.getId();
         return categoryService.getUserCategories(userId);
+    }
+
+    @GetMapping("/{categoryId}/transactions")
+    public List<TransactionResponse> getTransactionsByCategory(
+            @PathVariable Long categoryId,
+            @AuthenticationPrincipal CustomUserDetails userDetails) {
+
+        return transactionService.getTransactionsByCategory(userDetails.getId(), categoryId);
+    }
+
+    @GetMapping("/{categoryId}/total")
+    public BigDecimal getTotalSpentByCategory(
+            @PathVariable Long categoryId,
+            @AuthenticationPrincipal CustomUserDetails userDetails) {
+
+        return transactionService.getTotalSpentByCategory(userDetails.getId(), categoryId);
+
+
     }
 }
 
